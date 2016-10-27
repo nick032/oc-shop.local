d7f53a7a578993ab94a87c4c5f7089af5bd9261c
nick032 <nick.x.a.k@gmail.com>
2016-10-27 20:11:56 +0300

    instal opencart

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.local/admin/');
+define('HTTP_CATALOG', 'http://opencart.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.local/admin/');
+define('HTTPS_CATALOG', 'http://opencart.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ninja/www/opencart.local/admin/');
+define('DIR_SYSTEM', '/home/ninja/www/opencart.local/system/');
+define('DIR_IMAGE', '/home/ninja/www/opencart.local/image/');
+define('DIR_LANGUAGE', '/home/ninja/www/opencart.local/admin/language/');
+define('DIR_TEMPLATE', '/home/ninja/www/opencart.local/admin/view/template/');
+define('DIR_CONFIG', '/home/ninja/www/opencart.local/system/config/');
+define('DIR_CACHE', '/home/ninja/www/opencart.local/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ninja/www/opencart.local/system/storage/download/');
+define('DIR_LOGS', '/home/ninja/www/opencart.local/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ninja/www/opencart.local/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ninja/www/opencart.local/system/storage/upload/');
+define('DIR_CATALOG', '/home/ninja/www/opencart.local/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/ninja/www/opencart.local/catalog/');
+define('DIR_SYSTEM', '/home/ninja/www/opencart.local/system/');
+define('DIR_IMAGE', '/home/ninja/www/opencart.local/image/');
+define('DIR_LANGUAGE', '/home/ninja/www/opencart.local/catalog/language/');
+define('DIR_TEMPLATE', '/home/ninja/www/opencart.local/catalog/view/theme/');
+define('DIR_CONFIG', '/home/ninja/www/opencart.local/system/config/');
+define('DIR_CACHE', '/home/ninja/www/opencart.local/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/ninja/www/opencart.local/system/storage/download/');
+define('DIR_LOGS', '/home/ninja/www/opencart.local/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/ninja/www/opencart.local/system/storage/modification/');
+define('DIR_UPLOAD', '/home/ninja/www/opencart.local/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
